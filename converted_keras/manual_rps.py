d4e2732e21eb42959c9439fafc28f20abdab8da3
jailathia <jailathia@gmail.com>
2022-08-15 05:57:58 +0100

    comp choice defined

---
+++ converted_keras/manual_rps.py
@@ -0,0 +1,11 @@
+import random
+
+def get_computer_choice(comp_choice, self)
+
+    comp_choice = random.choice(["Rock", "Paper", "Scissors"])
+
+pass
+
+def get_user_choice(user_choice, self)
+
+pass

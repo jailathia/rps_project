a87686b0dda77da835907ec6f59d483bce14faa3
jailathia <jailathia@gmail.com>
2022-08-15 20:20:57 +0100

    manual first completed

--- converted_keras/manual_rps.py
+++ converted_keras/manual_rps.py
@@ -46,7 +46,12 @@ def get_winner(computer_choice, user_choice):
             return result[0]
 
 def play():
+    
     while True:
         computer_choice = get_computer_choice()
         user_choice = get_user_choice()
         get_winner(computer_choice, user_choice)
+        break
+
+play()
+    

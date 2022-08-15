7d1dc1e0c4e0610dbc3f2f157997bee030eaf365
jailathia <jailathia@gmail.com>
2022-08-15 06:27:19 +0100

    user choice failsafe

--- converted_keras/manual_rps.py
+++ converted_keras/manual_rps.py
@@ -7,7 +7,13 @@ def get_computer_choice(comp_choice, self)
     return comp_choice
 
 def get_user_choice(user_choice, self)
-
-    self.user_choice = input("Choose from: \n 1. rock \n 2. paper \n 3. scissors \n Type the corresponding number: ")
-
+    while True
+        user_choice = input("Choose from: \n 1. rock \n 2. paper \n 3. scissors \n Type the corresponding number: ")
+        if user_choice not in ["1", "2", "3"]
+            print("Please enter 1, 2 or 3")
+        else:
+            break
+    
+    self.user_choice = user_choice
+    
     return user_choice

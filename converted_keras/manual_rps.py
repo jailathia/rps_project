10e861803ebd15731af6ee6caa9c92c0a08d9ada
jailathia <jailathia@gmail.com>
2022-08-15 06:31:20 +0100

    removed self from definitions

--- converted_keras/manual_rps.py
+++ converted_keras/manual_rps.py
@@ -1,19 +1,18 @@
 import random
 
-def get_computer_choice(comp_choice, self)
+def get_computer_choice():
 
-    self.comp_choice = random.choice(["rock", "paper", "scissors"])
+    comp_choice = random.choice(["rock", "paper", "scissors"])
 
     return comp_choice
 
-def get_user_choice(user_choice, self)
-    while True
-        user_choice = input("Choose from: \n 1. rock \n 2. paper \n 3. scissors \n Type the corresponding number: ")
-        if user_choice not in ["1", "2", "3"]
-            print("Please enter 1, 2 or 3")
+def get_user_choice():
+    
+    while True:
+        user_choice = input("Choose from: \n 1. rock \n 2. paper \n 3. scissors \n Enter the corresponding number: ")
+        if user_choice not in ["1", "2", "3"]:
+            print("Please choose 1, 2 or 3")
         else:
             break
-    
-    self.user_choice = user_choice
     
     return user_choice
